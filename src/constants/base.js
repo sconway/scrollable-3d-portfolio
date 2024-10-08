2c930b4188c7c08cfcefd60d13de9a1e37152fd2
Scott Conway <scottconway@Scotts-MacBook-Air.local>
2024-10-08 19:13:14 -0400

    space

--- src/constants/base.js
+++ src/constants/base.js
@@ -3,7 +3,7 @@ export const SCENE_SIZE = 200
 export const PLANE_SIZE = 10000
 export const CURVE_PATH_HEIGHT = 4
 export const END_POINT = -SCENE_SIZE * 5 - 100
-
+ 
 export const ABOUT_THRESHOLD = 0.034
 export const SKILLS_GRAPH_TEXT_THRESHOLD = 0.054
 export const SKILLS_CLOUD_TEXT_THRESHOLD = 0.098
